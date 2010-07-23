54c8d2d189b757104b400d78cd7fff52c149c149
robbo <dcchut@gmail.com>
2010-07-23 11:02:23 +1000

    updated sql

--- sc2.sql
+++ sc2.sql
@@ -1,35 +1,35 @@
 Begin Transaction;
 
-Create  TABLE MAIN.[maps](
+Create  TABLE maps (
 [name] varchar(80) UNIQUE NOT NULL
 ,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
    
 );
 
-Create  TABLE MAIN.[players](
+Create  TABLE players (
 [name] varchar(40) UNIQUE NOT NULL
 ,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
    
 );
 
-Create  TABLE MAIN.[players_replays](
+Create  TABLE players_replays (
 [replay_id] integer(8)
 ,[player_id] integer(8)
 ,[race_id] integer(8)
    
 );
 
-Create  TABLE MAIN.[races](
+Create  TABLE races (
 [name] varchar(20) UNIQUE NOT NULL
 ,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
    
 );
 
-INSERT INTO MAIN.[races] ('name') VALUES ('Zerg');
-INSERT INTO MAIN.[races] ('name') VALUES ('Protoss');
-INSERT INTO MAIN.[races] ('name') VALUES ('Terran');
+INSERT INTO races ('name') VALUES ('Zerg');
+INSERT INTO races ('name') VALUES ('Protoss');
+INSERT INTO races ('name') VALUES ('Terran');
 
-Create  TABLE MAIN.[replays](
+Create  TABLE replays (
 [filename] varchar(200) NOT NULL
 ,[upload_date] integer(8) NOT NULL
 ,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
