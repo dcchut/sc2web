56faa64d0f4ad6b04f11c5ca137e1753c664aecb
robbo <dcchut@gmail.com>
2010-07-23 14:04:13 +1000

    added more CGI hax

--- modules/starparse/classes/kohana/starparse.php
+++ modules/starparse/classes/kohana/starparse.php
@@ -32,18 +32,26 @@ class Kohana_Starparse {
         // are we using the dodgy cgi key?
         if (isset($config['cgi_key']))
         {
-            return trim(@file_get_contents($config['cgi_key'] . '&replay=' . urlencode($archive_name) . '&internal=' . urlencode($internal_file_name)));
-            
-        }
-        
-        // otherwise do it through the shell
-        $exec = isset($config['readfile']) ? $config['readfile'] : 'readfile';
-        $cmd = $exec . " " . escapeshellarg($archive_name) . " " . escapeshellarg($internal_file_name) . " q";
+            $url = $config['cgi_key'] . '&replay=' . $archive_name . '&internal=' . $internal_file_name;
 
-        // catch the output of this command
-        ob_start(); {
-            passthru($cmd);
-        } $ret = ob_get_clean();
+            if (isset($config['cgi_replace'])) 
+            {
+                $url = str_replace($config['cgi_replace'][0], $config['cgi_replace'][1]);
+            }
+            
+            $ret = trim(@file_get_contents($url));
+        }
+        else 
+        {
+            // otherwise do it through the shell
+            $exec = isset($config['readfile']) ? $config['readfile'] : 'readfile';
+            $cmd = $exec . " " . escapeshellarg($archive_name) . " " . escapeshellarg($internal_file_name) . " q";
+    
+            // catch the output of this command
+            ob_start(); {
+                passthru($cmd);
+            } $ret = trim(ob_get_clean());
+        }
 
         if ($ret === 'error')
             return FALSE;
