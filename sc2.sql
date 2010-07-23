0b56fcbd42c7b60b3cba30f8dd279081e8c34314
robbo <dcchut@gmail.com>
2010-07-23 11:04:27 +1000

    updated sql again

--- sc2.sql
+++ sc2.sql
@@ -1,28 +1,24 @@
 Begin Transaction;
 
 Create  TABLE maps (
-[name] varchar(80) UNIQUE NOT NULL
-,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
-   
+	id integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+	name varchar(80) UNIQUE NOT NULL
 );
 
 Create  TABLE players (
-[name] varchar(40) UNIQUE NOT NULL
-,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
-   
+id integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+name varchar(40) UNIQUE NOT NULL  
 );
 
 Create  TABLE players_replays (
 [replay_id] integer(8)
 ,[player_id] integer(8)
 ,[race_id] integer(8)
-   
 );
 
 Create  TABLE races (
-[name] varchar(20) UNIQUE NOT NULL
-,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
-   
+id integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+name varchar(20) UNIQUE NOT NULL
 );
 
 INSERT INTO races ('name') VALUES ('Zerg');
