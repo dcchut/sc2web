f19f5b691572c504b9eaeb16c73e02f0051d51ce
robbo <dcchut@gmail.com>
2010-07-23 11:07:04 +1000

    updated sql again again

--- sc2.sql
+++ sc2.sql
@@ -1,23 +1,23 @@
 Begin Transaction;
 
 Create  TABLE maps (
-	id integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name varchar(80) UNIQUE NOT NULL
 );
 
 Create  TABLE players (
-id integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-name varchar(40) UNIQUE NOT NULL  
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name varchar(40) UNIQUE NOT NULL  
 );
 
 Create  TABLE players_replays (
-[replay_id] integer(8)
-,[player_id] integer(8)
-,[race_id] integer(8)
+replay_id integer(8),
+player_id integer(8),
+race_id integer(8)
 );
 
 Create  TABLE races (
-id integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+id integer PRIMARY KEY AUTOINCREMENT,
 name varchar(20) UNIQUE NOT NULL
 );
 
@@ -26,13 +26,13 @@ INSERT INTO races ('name') VALUES ('Protoss');
 INSERT INTO races ('name') VALUES ('Terran');
 
 Create  TABLE replays (
-[filename] varchar(200) NOT NULL
-,[upload_date] integer(8) NOT NULL
-,[id] integer PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL
-,[user_id] integer(8) NOT NULL
-,[hash] varchar(40) UNIQUE NOT NULL
-,[downloaded] integer(8) NOT NULL
-,[map_id] integer(8) NOT NULL
+filename varchar(200) NOT NULL,
+upload_date integer(8) NOT NULL,
+id integer PRIMARY KEY AUTOINCREMENT,
+user_id integer(8) NOT NULL,
+hash varchar(40) UNIQUE NOT NULL,
+downloaded integer(8) NOT NULL,
+map_id integer(8) NOT NULL
 );
 
 Commit Transaction;
