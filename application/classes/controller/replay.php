83162d4c345dfc76da6643a2a2c1686569433963
robbo <dcchut@gmail.com>
2010-07-23 11:36:52 +1000

    debug

--- application/classes/controller/replay.php
+++ application/classes/controller/replay.php
@@ -35,7 +35,7 @@ class Controller_Replay extends Controller_Site {
         /*
          * Are we uploading a MASS of ZIP files?
 		 */
-        if ($zip = Archive::factory('zip')->open($_FILES['file']['tmp_name']))
+        if (($zip = Archive::factory('zip')->open($_FILES['file']['tmp_name'])) !== FALSE)
         {
             foreach ($zip->file_list() as $zfile)
             {
