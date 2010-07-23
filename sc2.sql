7fe93c48806f5a5815a4d93e5510f5b8723a7bef
robbo <dcchut@gmail.com>
2010-07-23 11:11:02 +1000

    final sql update

--- sc2.sql
+++ sc2.sql
@@ -1,12 +1,12 @@
 Begin Transaction;
 
 Create  TABLE maps (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	name varchar(80) UNIQUE NOT NULL
 );
 
 Create  TABLE players (
-	id integer PRIMARY KEY AUTOINCREMENT,
+	id integer PRIMARY KEY,
 	name varchar(40) UNIQUE NOT NULL  
 );
 
@@ -17,7 +17,7 @@ race_id integer(8)
 );
 
 Create  TABLE races (
-id integer PRIMARY KEY AUTOINCREMENT,
+id integer PRIMARY KEY,
 name varchar(20) UNIQUE NOT NULL
 );
 
@@ -28,7 +28,7 @@ INSERT INTO races ('name') VALUES ('Terran');
 Create  TABLE replays (
 filename varchar(200) NOT NULL,
 upload_date integer(8) NOT NULL,
-id integer PRIMARY KEY AUTOINCREMENT,
+id integer PRIMARY KEY,
 user_id integer(8) NOT NULL,
 hash varchar(40) UNIQUE NOT NULL,
 downloaded integer(8) NOT NULL,
