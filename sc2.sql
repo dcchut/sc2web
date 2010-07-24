44d9eb23af503b0273835fb9a3a56290ca9558a8
robbo <dcchut@gmail.com>
2010-07-25 02:22:16 +1000

    updated sql file to allow for a complete wipe of the database upon import

--- sc2.sql
+++ sc2.sql
@@ -1,31 +1,24 @@
-Begin Transaction;
+BEGIN TRANSACTION;
 
-Create  TABLE maps (
+-- kill all the tables which already exist
+DROP TABLE IF EXISTS maps;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS replays;
+DROP TABLE IF EXISTS players_replays;
+DROP TABLE IF EXISTS races;
+
+-- create necessary tables
+CREATE TABLE maps (
 	id INTEGER PRIMARY KEY,
 	name varchar(80) UNIQUE NOT NULL
 );
 
-Create  TABLE players (
+CREATE TABLE players (
 	id integer PRIMARY KEY,
 	name varchar(40) UNIQUE NOT NULL  
 );
 
-Create  TABLE players_replays (
-replay_id integer(8),
-player_id integer(8),
-race_id integer(8)
-);
-
-Create  TABLE races (
-id integer PRIMARY KEY,
-name varchar(20) UNIQUE NOT NULL
-);
-
-INSERT INTO races ('name') VALUES ('Zerg');
-INSERT INTO races ('name') VALUES ('Protoss');
-INSERT INTO races ('name') VALUES ('Terran');
-
-Create  TABLE replays (
+CREATE TABLE replays (
 filename varchar(200) NOT NULL,
 upload_date integer(8) NOT NULL,
 id integer PRIMARY KEY,
@@ -35,4 +28,20 @@ downloaded integer(8) NOT NULL,
 map_id integer(8) NOT NULL
 );
 
-Commit Transaction;
+CREATE TABLE players_replays (
+replay_id integer(8),
+player_id integer(8),
+race_id integer(8)
+);
+
+CREATE TABLE races (
+id integer PRIMARY KEY,
+name varchar(20) UNIQUE NOT NULL
+);
+
+-- insert our trusty races
+INSERT INTO races ('name') VALUES ('Zerg');
+INSERT INTO races ('name') VALUES ('Protoss');
+INSERT INTO races ('name') VALUES ('Terran');
+
+COMMIT TRANSACTION;
